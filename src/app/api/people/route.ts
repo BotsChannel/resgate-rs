78722eadabbe670beeb205f9710b3eef3c6ccde4
lafayettegabe <gabriel-soares@uergs.edu.br>
2024-05-05 19:00:09 -0300

    chore: put person

--- src/app/api/people/route.ts
+++ src/app/api/people/route.ts
@@ -1,4 +1,4 @@
-import { getPeople, postPerson } from "@/lib/prisma/queries/people";
+import { getPeople, postPerson, updatePerson } from "@/lib/prisma/queries/people";
 import { NextRequest } from "next/server";
 
 export async function GET(
@@ -51,3 +51,51 @@ export async function POST(
         }, { status: 500 });
     }
 }
+
+export async function PUT(
+    req: NextRequest,
+) {
+    try {
+        const body = await req.json();
+        const person = {
+            id: body.id,
+            name: body.name,
+            age: body.age,
+            status: body.status,
+            photoUrl: body.photoUrl,
+            cidade: body.cidade,
+            endereco: body.endereco,
+            abrigo: body.abrigo,
+            entrada: body.entrada,
+        }
+
+        const response = await updatePerson({
+            id: person.id,
+            name: person.name,
+            age: person.age,
+            status: person.status,
+            photoUrl: person.photoUrl,
+            cidade: person.cidade,
+            endereco: person.endereco,
+            abrigo: person.abrigo,
+            entrada: person.entrada,
+        });
+
+        if (response) {
+            return Response.json({
+                message: "Person updated successfully",
+                data: response
+            }, { status: 200 });
+        } else {
+            return Response.json({
+                message: "Error updating person"
+            }, { status: 500 });
+        }
+    } catch (error: any) {
+        console.error("Error updating person: ", error.message);
+        return Response.json({
+            message: "Error updating person"
+        }, { status: 500 });
+    }
+}
+        
